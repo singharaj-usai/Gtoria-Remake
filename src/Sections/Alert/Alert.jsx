0a24645f93a8c5fddbe0037cd6a09e0b075874ba
Singharaj Usai <djjd22938@gmail.com>
2023-04-11 16:58:12 -0400

    fix discord link hash router

--- src/Sections/Alert/Alert.jsx
+++ src/Sections/Alert/Alert.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function Alert() {
   return (
     <div>
       <div className="alert alert-warning alert-dismissible fade show rounded-0 mb-0 text-center" role="alert">
-        Join our <Link className="alert-link" to="https://www.discord.gg/jyjHvWB34T" target="_blank">Discord server</Link> to get updates on the website.
+        Join our <a className="alert-link" href="https://www.discord.gg/jyjHvWB34T" target="_blank" rel="noopener noreferrer">Discord server</a> to get updates on the website.
         <b> This site is a heavy work in progress. </b>
       </div>
     </div>
--- src/Sections/Footer/Footer.jsx
+++ src/Sections/Footer/Footer.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
         <footer className="bg-light text-center text-lg-start position-absolute bottom-0 w-100">
-            <div className="container py-4">
+            <div className="container py-3">
                 <div className="text-center">
                     <h3 className="text-muted">Graphictoria</h3>
                     <div className=''>
                         &copy; {new Date().getFullYear()} Graphictoria
                         <br />
-                        <a href="/terms" className="text-primary text-white">Terms of Service</a>
+                        <Link to="/terms" className="text-primary text-white">Terms of Service</Link>
                     </div>
                 </div>
             </div>
