c332d5e67dbc6ceb2fe492b4cb6c843cc9bef879
Singharaj Usai <djjd22938@gmail.com>
2023-04-11 12:50:59 -0400

    forums data placeholder

--- src/Components/Forum/Forum.jsx
+++ src/Components/Forum/Forum.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ForumTableRow(props) {
   const { name, description, posts, replies } = props;
-  
+
   return (
     <tr>
       <td>{name}</td>
@@ -15,7 +15,7 @@ function ForumTableRow(props) {
 
 function ForumTable(props) {
   const { rows } = props;
-  
+
   return (
     <table className="table table-hover table-striped">
       <thead>
@@ -34,34 +34,52 @@ function ForumTable(props) {
 }
 
 function ListGroupItem(props) {
-  const { active, children } = props;
-  
+  const { active, children, onClick } = props;
+
   return (
-    <a href="#" className={`list-group-item list-group-item-action ${active ? 'active' : ''}`}>
+    <button className={`list-group-item list-group-item-action ${active ? 'active' : ''}`} onClick={onClick}>
       {children}
-    </a>
+    </button>
   );
 }
 
 function Forum() {
-  const rows = [
-    { name: 'General Discussion', description: 'Discuss anything related to our app', posts: 20, replies: 50 },
-    { name: 'Bugs & Issues', description: 'Report any bugs or issues you encounter', posts: 5, replies: 10 },
-    { name: 'Feedback & Suggestions', description: 'Share your feedback and ideas for the app', posts: 10, replies: 25 },
-    { name: 'Off-Topic', description: 'Discuss anything not related to the app', posts: 15, replies: 30 },
-  ];
+  const [activeItem, setActiveItem] = useState('General Discussion');
+
+  const forumTables = {
+    'General Discussion': [
+      { name: 'All Things ROBLOX', description: 'The area for discussions purely about ROBLOX - the features, the games, and company news', posts: 7668774, replies: 1167125 },
+      { name: 'Help (Technical Support and Account Issues)', description: 'Seeking account or technical help? Post your questions here', posts: 1145300, replies: 261298 },
+      { name: 'Suggestions & Ideas', description: 'Share your feedback and ideas for the app', posts: 4419344, replies: 483979 },
+
+    ],
+    'Club Houses': [
+      { name: 'ROBLOX Talk', description: 'A popular hangout where ROBLOXians talk about various topics', posts: 35833518, replies: 5125526 },
+      { name: 'Off Topic', description: 'When no other forum makes sense for your post, Off Topic will help it make even less sense', posts: 36152595, replies: 5562879 },
+      { name: "Let's Make a Deal", description: 'A fast paced community dedicated to mastering the Limited Trades and Sales market, and divining the subtleties of the ROBLOX Currency Exchange', posts: 38208099, replies: 5742912 },
+    
+    ],
+  };
+
+  const handleItemClick = item => {
+    setActiveItem(item);
+  };
 
   return (
     <div className="container my-5">
       <h1 className="text-center mb-4">Forum</h1>
       <div className="row">
         <div className="col-md-8">
-          <ForumTable rows={rows} />
+          <ForumTable rows={forumTables[activeItem]} />
         </div>
         <div className="col-md-4">
           <div className="list-group">
-            <ListGroupItem active={true}>News</ListGroupItem>
-            <ListGroupItem active={false}>Changelogs</ListGroupItem>
+            <ListGroupItem active={activeItem === 'General Discussion'} onClick={() => handleItemClick('General Discussion')}>
+              General Discussion
+            </ListGroupItem>
+            <ListGroupItem active={activeItem === 'Club Houses'} onClick={() => handleItemClick('Club Houses')}>
+              Club Houses
+            </ListGroupItem>
           </div>
         </div>
       </div>
