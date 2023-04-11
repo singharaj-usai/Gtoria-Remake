5cd2d9688f4708755449cdbd123cc55c336ddaa7
Singharaj Usai <djjd22938@gmail.com>
2023-04-11 14:49:09 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Sections/Navbar/Navbar';
 import Buttons from './Components/Buttons/Buttons';
 import Forum from './Components/Forum/Forum';
@@ -13,17 +13,30 @@ function App() {
     { name: 'Games', url: '/games' },
     { name: 'Catalog', url: '/catalog' }
   ];
-  return (
-    <div className='bg-secondary'>
-      <Navbar navLinks={navLinks} />
-      <div className="container p-4">
 
-       
-        <Forum />
-        
+  return (
+    <Router>
+      <div className='bg-secondary'>
+        <Navbar navLinks={navLinks} />
+        <div className="container p-4">
+          <Switch>
+            <Route exact path="/">
+              <Buttons />
+            </Route>
+            <Route path="/forum">
+              <Forum />
+            </Route>
+            <Route path="/games">
+              <h1>Games</h1>
+            </Route>
+            <Route path="/catalog">
+              <h1>Catalog</h1>
+            </Route>
+          </Switch>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
