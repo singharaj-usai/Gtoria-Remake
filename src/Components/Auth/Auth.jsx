aca5216d4833c7f3d62efe6c1c92efedf58634d8
Singharaj Usai <djjd22938@gmail.com>
2023-04-14 20:21:30 -0400

    login signup activetab state

--- src/Components/Auth/Auth.jsx
+++ src/Components/Auth/Auth.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 import 'bootswatch/dist/lumen/bootstrap.min.css';
 
 function Auth() {
@@ -9,10 +9,17 @@ function Auth() {
         confirmPassword: '',
     });
 
-    const { username, email, password, confirmPassword } = formData;
+    const [activeTab, setActiveTab] = useState('register');
+
+
+    const toggleTab = (tab) => {
+        if (activeTab !== tab) setActiveTab(tab);
+    };
+
+    const {username, email, password, confirmPassword} = formData;
 
     const onChange = (e) =>
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+        setFormData({...formData, [e.target.name]: e.target.value});
 
     const onSubmit = (e) => {
         e.preventDefault();
@@ -21,8 +28,31 @@ function Auth() {
 
     return (
         <div className="container p-5">
+
             <div className="row">
+
                 <div className="col-md-6 offset-md-3">
+                    <ul className="nav nav-tabs nav-justified mb-4">
+                        <li className="nav-item">
+                            <button
+                                className={`nav-link ${activeTab === 'register' ? 'active' : ''} w-100`}
+                                onClick={() => toggleTab('register')}
+                            >
+                                Register
+                            </button>
+                        </li>
+                        <li className="nav-item">
+                            <button
+                                className={`nav-link ${activeTab === 'login' ? 'active' : ''} w-100`}
+                                onClick={() => toggleTab('login')}
+                            >
+                                Login
+                            </button>
+                        </li>
+                    </ul>
+
+
+                    {activeTab === 'register' ? (
                     <div className="card shadow-sm">
                         <div className="card-header bg-primary text-white text-center">
                             <h3 className="card-title mb-0">Register</h3>
@@ -93,11 +123,53 @@ function Auth() {
                                     </button>
                                 </div>
                             </form>
-                            <div className="mt-3">
-                                Already have an account? <Link to="/login">Login</Link>
-                            </div>
                         </div>
                     </div>
+                    ) : (
+                        <div className="card shadow-sm">
+                            <div className="card-header bg-primary text-white text-center">
+                                <h3 className="card-title mb-0">Login</h3>
+                            </div>
+                            <div className="card-body">
+                                <form onSubmit={(e) => onSubmit(e)}>
+                                    <div className="form-group mb-3">
+                                        <label htmlFor="username" className="form-label">
+                                            Username
+                                        </label>
+                                        <input
+                                            type="username"
+                                            className="form-control"
+                                            id="username"
+                                            name="username"
+                                            value={username}
+                                            onChange={(e) => onChange(e)}
+                                            required
+                                        />
+                                    </div>
+                                    <div className="form-group mb-3">
+                                        <label htmlFor="password" className="form-label">
+                                            Password
+                                        </label>
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            id="password"
+                                            name="password"
+                                            value={password}
+                                            onChange={(e) => onChange(e)}
+                                            minLength="6"
+                                            required
+                                        />
+                                    </div>
+                                    <div className="d-grid gap-2 mt-4">
+                                        <button className="btn btn-success btn-block" type="submit">
+                                            Login
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
